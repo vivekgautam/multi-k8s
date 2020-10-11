8a94f0e2eaaf633e5606d1a72f554459dff164f2
Vivek Gautam <vivek.gautam@pointclickcare.com>
2020-10-11 17:25:20 -0400

    modified travis.yml and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t vivekgautam/multi-client:latest -t vivekgautam/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t vivekgautam/multi-server:latest -t vivekgautam/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t vivekgautam/multi-worker:latest -t vivekgautam/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push vivekgautam/multi-client:latest
+docker push vivekgautam/multi-server:latest
+docker push vivekgautam/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push vivekgautam/multi-client:$SHA
+docker push vivekgautam/multi-server:$SHA
+docker push vivekgautam/multi-worker:$SHA
 
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+# kubectl apply -f k8s
+# kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+# kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+# kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
