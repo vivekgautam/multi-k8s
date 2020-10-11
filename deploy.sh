d9f4f556aa531062d2ad9762c3fe08192191f9d3
Vivek Gautam <vivek.gautam@pointclickcare.com>
2020-10-11 17:37:36 -0400

    fixed workspace name

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-docker build -t vivekgautam/multi-client:latest -t vivekgautam/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t vivekgautam/multi-server:latest -t vivekgautam/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t vivekgautam/multi-worker:latest -t vivekgautam/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t kviev/multi-client:latest -t kviev/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kviev/multi-server:latest -t kviev/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kviev/multi-worker:latest -t kviev/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push vivekgautam/multi-client:latest
-docker push vivekgautam/multi-server:latest
-docker push vivekgautam/multi-worker:latest
+docker push kviev/multi-client:latest
+docker push kviev/multi-server:latest
+docker push kviev/multi-worker:latest
 
-docker push vivekgautam/multi-client:$SHA
-docker push vivekgautam/multi-server:$SHA
-docker push vivekgautam/multi-worker:$SHA
+docker push kviev/multi-client:$SHA
+docker push kviev/multi-server:$SHA
+docker push kviev/multi-worker:$SHA
 
 # kubectl apply -f k8s
 # kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
