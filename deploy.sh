20f876844ce9214b59fba5b549bb9b16fc1e5cbf
Vivek Gautam <vivek.gautam@pointclickcare.com>
2020-10-11 20:37:46 -0400

    debugging

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ docker push kviev/multi-client:$SHA
 docker push kviev/multi-server:$SHA
 docker push kviev/multi-worker:$SHA
 
+
 sudo kubectl apply -f k8s
 sudo kubectl set image deployments/server-deployment server=kviev/multi-server:$SHA
 sudo kubectl set image deployments/client-deployment client=kviev/multi-client:$SHA
